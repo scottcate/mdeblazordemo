84569a58cdfe2009a08854922054ee9cb4f0d2a7
Scott Cate <Scottca@microsoft.com>
2018-04-19 20:57:26 -0500

    init

---
+++ MdeBlazor/Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MdeBlazor.Models
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
